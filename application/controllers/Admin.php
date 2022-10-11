e8cd4d9646d3b22caa25915495c611ee57ed3e51
Galang Hanafi <galanghanafi8@gmail.com>
2022-10-11 11:51:02 +0700

    Update files

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -57,7 +57,7 @@ class Admin extends CI_Controller
                 'name'          => htmlspecialchars($this->input->post('name', true)),
                 'email'         => htmlspecialchars($this->input->post('email', true)),
                 'image'         => 'default.jpg',
-                'password'      => $this->input->post('password1'),
+                'password'      => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id'       => 2,
                 'is_active'     => 1,
                 'date_created'  => time(),
